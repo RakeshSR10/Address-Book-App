6b20697ec1487a3bf0547d6357c0d159072a635c
RakeshSR10 <rakeshsrking@gmail.com>
2021-06-02 09:13:49 +0530

    Added: Ability to converting Address Book App to Client Server Architecture

--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,28 @@
-/*//UC 15 - Update Person Data
-const update = (node) => {
-    let addressBookData = addressBookList.find(personData => personData._id == node.id)
-    if(!addressBookData) return;
-    localStorage.setItem('editEmp', JSON.stringify(addressBookData));
-    window.location.replace(site_properties.add_person_page);
-}*/
+const checkName = (fullname) => {
+    let nameRegex = RegExp('^[A-Z_]{1}[a-zA-Z_ ]{3,}$');
+    if(!nameRegex.test(fullname)) {
+        throw 'Invalid Name';  
+    }
+}
+
+const checkPhone = (phone) => {
+    let phoneRegex1 = RegExp(/^[\+]?[(]?[0-9]{3}[)]?[0-9]{3}[-\s\.]?[0-9]{6,13}$/im);
+    
+    if(!phoneRegex1.test(phone)){ 
+        throw 'Invalid Phone Number';
+    } 
+}
+
+const checkAddress = (address) => {
+    let addressRegex = RegExp('^[a-zA-Z0-9_][a-zA-Z0-9_ ]{4,}$');
+    if(!addressRegex.test(address)){
+        throw 'Invalid Address';
+    }
+}
+
+const checkZip = (zip) => {
+    let zipRegex = RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$');
+    if(!zipRegex.test(zip)){
+        throw 'Invalid Zip-Code';
+    }    
+}
